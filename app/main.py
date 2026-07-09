4ea1e047c3914606f3ad2036632593fd6e0639e7
Carlos Eduardo <parraaa0901@gmail.com>
2026-07-09 07:48:23 -0700

    fix: alembic tables

--- app/main.py
+++ app/main.py
@@ -1,8 +1,5 @@
 from fastapi import FastAPI
-from app.database import engine, Base
 from app.routes import products, transactions
-
-Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Tienda Mamá")
 
